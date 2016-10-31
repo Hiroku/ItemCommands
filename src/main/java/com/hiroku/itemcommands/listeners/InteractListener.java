aebcdc631f437747c47effca8e53bd40ae1eeed4
Hiroku <Paul_L_Preston@hotmail.com>
2016-10-31 12:48:47 +1000

    Client side and off-hand events ignored

--- src/main/java/com/hiroku/itemcommands/listeners/InteractListener.java
+++ src/main/java/com/hiroku/itemcommands/listeners/InteractListener.java
@@ -3,6 +3,7 @@ package com.hiroku.itemcommands.listeners;
 import com.hiroku.itemcommands.ItemCommands;
 import com.hiroku.itemcommands.timertasks.CommandTask;
 
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -11,12 +12,16 @@ public class InteractListener
 	@SubscribeEvent
 	public void onItemRightClick(PlayerInteractEvent.RightClickItem event)
 	{
+		if (event.getWorld().isRemote || event.getHand() == EnumHand.OFF_HAND)
+			return;
 		ItemCommands.timer.schedule(new CommandTask(event.getItemStack().getItem(), event.getEntityPlayer().getName()), 0);
 	}
 	
 	@SubscribeEvent
 	public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
 	{
+		if (event.getWorld().isRemote || event.getHand() == EnumHand.OFF_HAND)
+			return;
 		if (event.getItemStack() != null)
 			ItemCommands.timer.schedule(new CommandTask(event.getItemStack().getItem(), event.getEntityPlayer().getName()), 0);
 	}
