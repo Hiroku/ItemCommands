38b5ef9d856a693b524431d8e0e3f511925b0303
Hiroku <Paul_L_Preston@hotmail.com>
2016-12-06 12:40:01 +1000

    Increment minor version to 1.1.0

--- src/main/java/com/hiroku/itemcommands/ItemCommands.java
+++ src/main/java/com/hiroku/itemcommands/ItemCommands.java
@@ -17,7 +17,7 @@ public class ItemCommands
 {
 	public static final String MODID = "itemcommands";
 	public static final String NAME = "Item Commands";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	
 	public static final Timer timer = new Timer();
 	
