4f5c67c28a49117cf40edda5713e42bf01b1d40a
Hiroku <Paul_L_Preston@hotmail.com>
2017-01-03 20:09:45 +1000

    Increment major version to 2.0.0

--- src/main/java/com/hiroku/itemcommands/ItemCommands.java
+++ src/main/java/com/hiroku/itemcommands/ItemCommands.java
@@ -26,7 +26,7 @@ public class ItemCommands
 {
 	public static final String MODID = "itemcommands";
 	public static final String NAME = "Item Commands";
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "2.0.0";
 	
 	public static final Timer timer = new Timer();
 	
