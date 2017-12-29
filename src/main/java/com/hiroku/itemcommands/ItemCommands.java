24f1e3edd75389d87e9ea3c853954bbf0028de4a
Paul <Paul_L_Preston@hotmail.com>
2017-12-29 19:46:18 +1000

    Moved dependency to API7, version 2.2.0

--- src/main/java/com/hiroku/itemcommands/ItemCommands.java
+++ src/main/java/com/hiroku/itemcommands/ItemCommands.java
@@ -26,7 +26,7 @@ public class ItemCommands
 {
 	public static final String MODID = "itemcommands";
 	public static final String NAME = "Item Commands";
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.2.0";
 	
 	public static final Timer timer = new Timer();
 	
